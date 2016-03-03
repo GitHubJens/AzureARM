4999cdf882cccf2f89356c359e6b41e22e479a4c
haavardg <havard.grondal@gmail.com>
2016-03-03 09:19:15 +0100

    Fix DSC

--- IaaS-NewActiveDirectory/DSC/Configure.ps1
+++ IaaS-NewActiveDirectory/DSC/Configure.ps1
@@ -1,7 +1,7 @@
-configuration CreatePDC 
-{ 
-   param 
-   ( 
+configuration CreateADPDC
+{
+   param
+   (
         [Parameter(Mandatory)]
         [String]$DomainName,
 
@@ -10,10 +10,12 @@ configuration CreatePDC
 
         [Int]$RetryCount=20,
         [Int]$RetryIntervalSec=30
-    ) 
+    )
 
-    Import-DscResource -ModuleName xActiveDirectory,xStorage,xNetworking
+    Import-DscResource -ModuleName xActiveDirectory,xDisk, xNetworking, cDisk, PSDesiredStateConfiguration
     [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+    $Interface=Get-NetAdapter|Where Name -Like "Ethernet*"|Select-Object -First 1
+    $InterfaceAlias=$($Interface.Name)
 
     Node localhost
     {
@@ -23,42 +25,61 @@ configuration CreatePDC
             RebootNodeIfNeeded = $true
         }
 
-        WindowsFeature DNS 
-        { 
-            Ensure = "Present" 
+        WindowsFeature DNS
+        {
+            Ensure = "Present"
             Name = "DNS"
         }
-        
+
         WindowsFeature DnsTools
         {
             Ensure = "Present"
             Name = "RSAT-DNS-Server"
+            DependsOn = "[WindowsFeature]DNS"
         }
 
-        xDnsServerAddress DnsServerAddress 
-        { 
-            Address        = '127.0.0.1' 
-            InterfaceAlias = 'Ethernet'
+        xDnsServerAddress DnsServerAddress
+        {
+            Address        = '127.0.0.1'
+            InterfaceAlias = $InterfaceAlias
             AddressFamily  = 'IPv4'
+            DependsOn = "[WindowsFeature]DNS"
         }
+
+        Script script1
+        {
+      	    SetScript = {
+                Set-DnsServerDiagnostics -All $true
+                Write-Verbose -Verbose "Enabling DNS client diagnostics"
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+            DependsOn = "[WindowsFeature]DNS"
+        }
+
         xWaitforDisk Disk2
         {
-             DiskNumber = 2
-             RetryIntervalSec =$RetryIntervalSec
-             RetryCount = $RetryCount
+            DiskNumber = 2
+            RetryIntervalSec =$RetryIntervalSec
+            RetryCount = $RetryCount
         }
+
         xDisk ADDataDisk
         {
             DiskNumber = 2
             DriveLetter = "F"
-            FSLabel ="ADDisk"
+            FSLabel = "ADData"
+            DependsOn="[xWaitforDisk]Disk2"
         }
-        WindowsFeature ADDSInstall 
-        { 
-            Ensure = "Present" 
+
+        WindowsFeature ADDSInstall
+        {
+            Ensure = "Present"
             Name = "AD-Domain-Services"
-        }  
-        xADDomain FirstDS 
+            DependsOn="[xDisk]ADDataDisk"
+        }
+
+        xADDomain FirstDS
         {
             DomainName = $DomainName
             DomainAdministratorCredential = $DomainCreds
@@ -66,14 +87,65 @@ configuration CreatePDC
             DatabasePath = "F:\NTDS"
             LogPath = "F:\NTDS"
             SysvolPath = "F:\SYSVOL"
-        } 
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
    }
-} 
+}
 
-configuration CreateBDC
-{ 
-   param 
-    ( 
+configuration PrepareADBDC
+{
+   param
+    (
+        [Parameter(Mandatory)]
+        [String]$DNSServer,
+
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30
+    )
+
+    Import-DscResource -ModuleName  xDisk, cDisk, xNetworking
+    $Interface=Get-NetAdapter|Where Name -Like "Ethernet*"|Select-Object -First 1
+    $InterfaceAlias=$($Interface.Name)
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            ConfigurationMode = 'ApplyOnly'
+            RebootNodeIfNeeded = $true
+        }
+        xWaitforDisk Disk2
+        {
+                DiskNumber = 2
+                RetryIntervalSec =$RetryIntervalSec
+                RetryCount = $RetryCount
+        }
+        xDisk ADDataDisk
+        {
+            DiskNumber = 2
+            DriveLetter = "F"
+            FSLabel = "ADData"
+            DependsOn="[xWaitforDisk]Disk2"
+        }
+        WindowsFeature ADDSInstall
+        {
+            Ensure = "Present"
+            Name = "AD-Domain-Services"
+            DependsOn = "[xDisk]ADDataDisk"
+        }
+        xDnsServerAddress DnsServerAddress
+        {
+            Address        = $DNSServer
+            InterfaceAlias = $InterfaceAlias
+            AddressFamily  = 'IPv4'
+            DependsOn="[WindowsFeature]ADDSInstall"
+        }
+   }
+}
+
+configuration ConfigureADBDC
+{
+   param
+    (
         [Parameter(Mandatory)]
         [String]$DomainName,
 
@@ -82,63 +154,51 @@ configuration CreateBDC
 
         [Int]$RetryCount=20,
         [Int]$RetryIntervalSec=30
-    ) 
-    Import-DscResource -ModuleName xActiveDirectory,xStorage,xNetworking
-    
+    )
+
+    Import-DscResource -ModuleName xActiveDirectory, xDisk, cDisk
+
     [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
-   
+
     Node localhost
     {
         LocalConfigurationManager
         {
-            ConfigurationMode = 'ApplyOnly'
+       	    ConfigurationMode = 'ApplyOnly'
             RebootNodeIfNeeded = $true
         }
-
-        WindowsFeature DNS 
-        { 
-            Ensure = "Present" 
-            Name = "DNS"
-        }
-        
-        WindowsFeature DnsTools
+        xWaitForADDomain DscForestWait
         {
-            Ensure = "Present"
-            Name = "RSAT-DNS-Server"
+            DomainName = $DomainName
+            DomainUserCredential= $DomainCreds
+            RetryCount = $RetryCount
+            RetryIntervalSec = $RetryIntervalSec
         }
-
-        xDnsServerAddress DnsServerAddress 
-        { 
-            Address        = '127.0.0.1' 
-            InterfaceAlias = 'Ethernet'
-            AddressFamily  = 'IPv4'
-        }
-        xWaitforDisk Disk2
+        xADDomainController BDC
         {
-             DiskNumber = 2
-             RetryIntervalSec =$RetryIntervalSec
-             RetryCount = $RetryCount
-        }
-        xDisk ADDataDisk
-        {
-            DiskNumber = 2
-            DriveLetter = "F"
-            FSLabel ="ADDisk"
-
-        }
-        WindowsFeature ADDSInstall 
-        { 
-            Ensure = "Present" 
-            Name = "AD-Domain-Services"
-        }
-        xADDomainController BDC 
-        { 
-            DomainName = $DomainName 
-            DomainAdministratorCredential = $DomainCreds 
+            DomainName = $DomainName
+            DomainAdministratorCredential = $DomainCreds
             SafemodeAdministratorPassword = $DomainCreds
             DatabasePath = "F:\NTDS"
             LogPath = "F:\NTDS"
             SysvolPath = "F:\SYSVOL"
+            DependsOn = "[xWaitForADDomain]DscForestWait"
         }
-   }
-} 
+        Script script1
+        {
+            SetScript =
+            {
+                $dnsFwdRule = Get-DnsServerForwarder
+                if ($dnsFwdRule)
+                {
+                    Remove-DnsServerForwarder -IPAddress $dnsFwdRule.IPAddress -Force
+                }
+                Write-Verbose -Verbose "Removing DNS forwarding rule"
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+            DependsOn = "[xADDomainController]BDC"
+        }
+    }
+}
+ 
